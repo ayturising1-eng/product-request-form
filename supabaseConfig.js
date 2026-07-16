a6ac6330d5f0dfc0cce024b20eecbb610acf3ab7
ayturising1-eng <ayturising1@gmail.com>
2026-07-16 18:25:02 +0300

    Delete supabaseConfig.js

--- supabaseConfig.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  'use strict';
-
-  window.PulumurSupabaseConfig = Object.freeze({
-    url: 'https://vfpqebndjgjuzskfwcmy.supabase.co',
-    publishableKey: 'sb_publishable_y0PNE-OCClOdL-nRktcOqA_LwX8jvSa'
-  });
-})();
